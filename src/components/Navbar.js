f978e82c12152b4bbd22baa21f486528e6da5f62
shashidhar0302 <saithechinnu@gmail.com>
2023-11-04 01:28:03 -0400

    Added Navbar title router

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
 import '../index.css'
 
+
 const Navbar = () => {
-let url = "#"
   return (
     <div className="container-fluid mb-5">
       <nav className="navbar navbar-expand-md fixed-top py-0 container-fluid rounded-bottom">
-        <div className="container-fluid pt-2 px-2 justify-content-center" id="head-nv">
-          <p id="title">
-            <a href={url} className="navbar-brand text-light h1 title">
+        <Container fluid className=" pt-2 px-2 my-0 justify-content-center" id="head-nv">
+          <Link to="/" style={{textDecoration: 'none'}}>
+            <div className="navbar-brand text-light h1 title">
               SGMA
-            </a>
-          </p>
-        </div>
+            </div>
+            </Link>
+        </Container>
       </nav>
     </div>
   )
