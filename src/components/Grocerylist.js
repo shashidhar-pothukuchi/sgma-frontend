d3159a6b17c85c5090f80ab61371ee27079d0d9f
shashidhar0302 <saithechinnu@gmail.com>
2023-11-05 08:47:33 -0500

    Grocery list addition

--- src/components/Grocerylist.js
+++ src/components/Grocerylist.js
@@ -1,11 +1,52 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-const Grocerylist = (props) => {
-  return (
-    <div>
-    <h4>This is the List</h4>
-  </div>
-  )
-}
+import React from "react";
+import { Button, Container, Row, Col, Image, ListGroup } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export default Grocerylist
+const Grocerylist = ({ items }) => {
+  return (
+    <Container className="list-table">
+      <ListGroup>
+        {items.map((item) => (
+          <ListGroup.Item as="button" action>
+            <Container className="text-center">
+              <Row>
+                <Col>
+                  <Image src={item.imgSrc} height="50" width="50" fluid />
+                </Col>
+                <Col>Quantity to Buy</Col>
+                <Col>Inventory Quantity</Col>
+                <Col>Expiry Date</Col>
+              </Row>
+              <Row>
+                <Col>{item.name}</Col>
+                <Col>
+                  <h5>
+                    {item.expiryDate && new Date(item.expiryDate) < new Date()
+                      ? item.quantity
+                      : item.defquantity - item.quantity > 0
+                      ? item.defquantity - item.quantity
+                      : 0}
+                  </h5>
+                </Col>
+                <Col>
+                  <h5>{item.quantity}</h5>
+                </Col>
+                <Col>
+                  <h5
+                    className={
+                      item.expired === false ? "text-success" : "text-danger"
+                    }
+                  >
+                    {item.expiryDate}
+                  </h5>
+                </Col>
+              </Row>
+            </Container>
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+    </Container>
+  );
+};
+
+export default Grocerylist;
